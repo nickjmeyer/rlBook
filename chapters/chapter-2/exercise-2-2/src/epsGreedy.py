5fdedd1367187db051b35c59b09612598c109724
James Gilman <jfgilman@ncsu.edu>
2015-06-28 23:14:27 -0400

    basic greedy agent functions

--- chapters/chapter-2/exercise-2-2/src/epsGreedy.py
+++ chapters/chapter-2/exercise-2-2/src/epsGreedy.py
@@ -1,2 +1,33 @@
 
-# greedy agent - no exploring
+import random
+
+class Agent:
+    
+    def __init__(self, policy, numHandles):
+        self.policy = policy
+        self.numHandles = numHandles
+        self.rewardList = [0]*numHandles
+        self.tries = [0]*numHandles
+        self.selectionIndex = None
+    
+    def action(self):
+        ### pulls a handle based on the rewardList
+        self.selectionIndex = self.policy(self.rewardList)
+        return self.selectionIndex
+ 
+    def learn(self, reward):
+        ### updates rewardList
+        oldMean = self.rewardList[self.selectionIndex]
+        newMean = (oldMean*self.tries[selectionIndex] + reward) / (self.tries[selectionIndex] + 1)
+        self.rewardList[self.selectionIndex] = newMean
+        self.tries[selectionIndex] = self.tries[selectionIndex] + 1
+    
+def greedyPolicy(rewardList):    
+    ## picks the handle with the highest expected reward
+    ## if multiple handles have highest reward select randomly 
+    m = max(rewardList)
+    return random.choice([i for i,j in enumerate(rewardList) if j == m])
+
+
+
+
--- chapters/chapter-2/exercise-2-2/src/nArmBanditRun.py
+++ chapters/chapter-2/exercise-2-2/src/nArmBanditRun.py
@@ -3,4 +3,5 @@
 # takes in number of runs, agent and distribution 
 # returns results
 def run(Sys, Agent, runs = 1000):
-    
+     reward = Sys.handles[selectionIndex].pull()
+     Agent.learn(selectionIndex, reward)
